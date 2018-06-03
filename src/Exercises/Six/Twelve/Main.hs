d7148ffaba94f825479537727d60a21f0f319301
Gleb Ivanov <g.ivanov@fun-box.ru>
2018-06-03 13:44:34 +0400

    Implemented radix sort

--- src/Exercises/Six/Twelve/Main.hs
+++ src/Exercises/Six/Twelve/Main.hs
@@ -1,10 +1,34 @@
 module Exercises.Six.Twelve.Main (main) where
 
+import Data.Array
+
 main :: IO ()
-main = putStrLn.show $ radixSort generateNumbers
+main = putStrLn.show $ radixSort 3 (0, 9) generateInts
 
-generateNumbers :: [[Int]]
-generateNumbers = [[a, b, c] | a <- [1 .. 2], b <- [1 .. 2], c <- [1 .. 2]]
+type Value a =  [a]
+type Bucket a = [Value a]
+type Buckets a = Array a (Bucket a)
 
-radixSort :: (Ord a) => [[a]] -> [[a]]
-radixSort list = list
+generateInts :: [Value Int]
+generateInts = [[a, b, c] | a <- [1 .. 2], b <- [1 .. 2], c <- [1 .. 2]]
+
+radixSort :: (Ord a, Ix a) => Int -> (a, a) -> [Value a] -> [Value a]
+radixSort 0 _ values = values
+radixSort radixPlusOne sortRange values = radixSort radix sortRange sortedByRadix
+  where
+    radix = radixPlusOne - 1
+    sortedByRadix
+      | radix == 0 = concatenatedBuckets
+      | otherwise = reverse concatenatedBuckets
+      where
+        concatenatedBuckets = concatBuckets (makeBuckets sortRange radix values)
+
+concatBuckets :: (Ord a) => Buckets a -> [Value a]
+concatBuckets = concat.elems
+
+makeBuckets :: (Ord a, Ix a) => (a, a) -> Int -> [Value a] -> Buckets a
+makeBuckets sortRange radix values = accumArray accumFn [] sortRange assocs
+  where
+    accumFn values value = value:values
+    assocs = map makeAssoc values
+    makeAssoc value = (value !! radix, value)
